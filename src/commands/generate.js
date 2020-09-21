da2421605226054090e80bd1a4b390b56c549b0c
qinjialei <qinjialei@foxmail.com>
2020-09-21 21:43:12 +0800

    refactor

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -2,42 +2,43 @@ const fs = require('fs');
 const path = require('path');
 const { toUpperCaseFirstWord, generateFileByTemplate } = require('../utils');
 
-//   process.cwd() 当前执行程序的路径（执行命令行时候的路径,不是代码路径 例如 在根目录下执行 node ./xxx/xxx/a.js 则 cwd 返回的是 根目录地址 ）
-// __dirname: 代码存放的位置
-// process.execPath: 当前执行的node路径（如：/bin/node）
+const PATH = './pages';
+const ACTION_NAME = 'page';
+
+function copyTemplate(from, to, fileName) {
+  from = path.join(__dirname, '../templates', from);
+  const rawContent = fs.readFileSync(from, 'utf-8');
+  const finalContent = generateFileByTemplate(rawContent, {
+    fileName,
+  });
+  write(to, finalContent);
+}
+
+function write(path, str, mode) {
+  fs.writeFileSync(path, str);
+}
+
+// 新建目录
+function mkdir(path, fn) {
+  fs.mkdir(path, function (err) {
+    fn && fn();
+  });
+}
+
 function generatePage(fileName) {
-  if (!process.cwd().endsWith('/src')) {
-    console.error('请在项目的 src 目录下运行！');
-    return;
-  }
-
   fileName = toUpperCaseFirstWord(fileName);
-  function copyTemplate(from, to) {
-    from = path.join(__dirname, '../templates', from);
-    const rawContent = fs.readFileSync(from, 'utf-8');
-    const finalContent = generateFileByTemplate(rawContent, {
-      fileName,
-    });
-    write(to, finalContent);
-  }
-  function write(path, str, mode) {
-    fs.writeFileSync(path, str);
-  }
-  // 新建目录
-  function mkdir(path, fn) {
-    fs.mkdir(path, function (err) {
-      fn && fn();
-    });
-  }
-
-  var PATH = './pages';
   mkdir(PATH + `/${fileName}`, function () {
-    copyTemplate('page', PATH + `/${fileName}/${fileName}.tsx`);
+    copyTemplate(ACTION_NAME, PATH + `/${fileName}/${fileName}.tsx`, fileName);
   });
 }
 
 module.exports = (actionName, fileName, ...option) => {
-  if (actionName === 'page') {
+  if (actionName === ACTION_NAME) {
+    if (!process.cwd().endsWith('/src')) {
+      console.error('请在项目的 src 目录下运行！');
+      return;
+    }
+
     generatePage(fileName);
   }
   if (actionName === 'component') {
--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,15 @@
 const axios = require('axios');
 const Metalsmith = require('metalsmith'); // 引入静态网站生成器
 const Handlebars = require('handlebars'); // 引入模板引擎
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
   toUpperCaseFirstWord(str) {
     return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
   },
-
   generateFileByTemplate(template, data) {
-    // compile the template
     var content = Handlebars.compile(template)(data);
     return content;
-    // var template = Handlebars.compile('Handlebars <b>{{doesWhat}}</b>');
-    // execute the compiled template and print the output to the console
   },
 };
