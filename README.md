3ca4b33a0d2b5d04553b75e831433b157396620a
jialei qin <jialei.qin@thoughtworks.com>
2021-09-02 20:39:10 +0800

    1

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# arch-guard-cli 
+# pff-cli 
 > 快速生成符合项目风格的代码，类似 angular-cli
 
 ## 使用
 ```bash
-npm install arch-guard-cli -g
+yarn add pff-cli -g
 ```
 
 ## 常用命令(必须在项目的**src**目录下运行)
-- 创建页面：`ag g page Demo`
-- 创建基础组件：`ag g c -ba (ag generate component --basic)`
-- 创建业务组件：`ag g c -bu (ag generate component --business)`
+- 创建页面：`pff g page Demo`
+- 创建基础组件：`pff g c -ba (pff generate component --basic)`
+- 创建业务组件：`pff g c -bu (pff generate component --business)`
 
 <img src="./assets/创建页面.gif" />
 
@@ -19,4 +19,3 @@ npm install arch-guard-cli -g
 - `component => c` 
 - `--basic => -ba`
 - `--business => -bu`
- 
--- src/commands/generate.js
+++ src/commands/generate.js
@@ -1,115 +1,124 @@
 const fs = require('fs');
 const path = require('path');
 
-const { toUpperCaseFirstWord, generateFileByTemplate } = require('../utils');
+const {toUpperCaseFirstWord, generateFileByTemplate} = require('../utils');
 
 const FILE_PATH = {
-  pagesPath: './pages', //页面存放的路径 TODO: 处理首字母大小写
-  componentsPath: './components', //组件存放的路径，TODO: 处理首字母大小写
+    pagesPath: './pages', //页面存放的路径 TODO: 处理首字母大小写
+    componentsPath: './components', //组件存放的路径，TODO: 处理首字母大小写
 };
 
 const ACTION_NAME = 'page';
 
-function copyTemplate(from, to, fileName) {
-  from = path.join(__dirname, '../templates', from);
-  const rawContent = fs.readFileSync(from, 'utf-8');
-  const finalContent = generateFileByTemplate(rawContent, {
-    fileName,
-  });
-  write(to, finalContent);
+function copyTemplate(from, to, fileName, componentName) {
+    from = path.join(__dirname, '../templates', from);
+    const rawContent = fs.readFileSync(from, 'utf-8');
+    const finalContent = generateFileByTemplate(rawContent, {
+        fileName,
+        componentName
+    });
+    write(to, finalContent);
 }
 
 function write(path, str, mode) {
-  fs.writeFileSync(path, str);
+    fs.writeFileSync(path, str);
 }
 
 function generatePage(fileName) {
-  fileName = toUpperCaseFirstWord(fileName);
-  fs.mkdirSync(FILE_PATH.pagesPath + `/${fileName}`);
-  copyTemplate(
-    ACTION_NAME,
-    FILE_PATH.pagesPath + `/${fileName}/${fileName}.tsx`,
-    fileName
-  );
-  copyTemplate(
-    ACTION_NAME + 'Less',
-    FILE_PATH.pagesPath + `/${fileName}/${fileName}.less`,
-    fileName
-  );
-  fs.mkdirSync(FILE_PATH.pagesPath + `/${fileName}` + '/components');
+    fileName = toUpperCaseFirstWord(fileName);
+    fs.mkdirSync(FILE_PATH.pagesPath + `/${fileName}`);
+    copyTemplate(
+        ACTION_NAME,
+        FILE_PATH.pagesPath + `/${fileName}/${fileName}.tsx`,
+        fileName
+    );
+    copyTemplate(
+        ACTION_NAME + 'Less',
+        FILE_PATH.pagesPath + `/${fileName}/${fileName}.less`,
+        fileName
+    );
+    fs.mkdirSync(FILE_PATH.pagesPath + `/${fileName}` + '/components');
 }
 
 function generateComponent(fileName, componentOptions) {
-  const { basic, business } = componentOptions;
-  if (basic) {
-    generateComponentBasic(fileName);
-  } else if (business) {
-    generateComponentBusiness(fileName);
-  }
+    console.log("-> fileName", fileName);
+    const {basic, business} = componentOptions;
+    console.log("-> basic", basic);
+    generateComponentBasic(fileName)
+
+    // if (basic) {
+    //   generateComponentBasic(fileName);
+    // } else if (business) {
+    //   generateComponentBusiness(fileName);
+    // }
 }
 
 function generateComponentBasic(fileName) {
-  fileName = toUpperCaseFirstWord(fileName);
-  fs.mkdirSync(FILE_PATH.componentsPath + '/Basic' + `/${fileName}`);
+    console.log("-> fileName", fileName);
+    // fs.mkdirSync(FILE_PATH.componentsPath  + `/${fileName}`);
+    fs.mkdirSync(fileName)
+    const componentName = toUpperCaseFirstWord(fileName)
 
-  copyTemplate(
-    `componentBasic`,
-    FILE_PATH.componentsPath + '/Basic' + `/${fileName}/${fileName}.tsx`,
-    fileName
-  );
-
-  copyTemplate(
-    'componentBasicLess',
-    FILE_PATH.componentsPath + '/Basic' + `/${fileName}/${fileName}.less`,
-    fileName
-  );
+    copyTemplate(
+        `componentBasic`,
+        `${fileName}/${componentName}.tsx`,
+        fileName,
+        componentName
+    );
+    //
+    // copyTemplate(
+    //   'componentBasicLess',
+    //   FILE_PATH.componentsPath + `/${fileName}/${fileName}.less`,
+    //   fileName
+    // );
 }
 
 function generateComponentBusiness(fileName) {
-  fileName = toUpperCaseFirstWord(fileName);
-  fs.mkdirSync(FILE_PATH.componentsPath + '/Business' + `/${fileName}`);
-  copyTemplate(
-    `componentBusiness`,
-    FILE_PATH.componentsPath + '/Business' + `/${fileName}/${fileName}.tsx`,
-    fileName
-  );
+    fileName = toUpperCaseFirstWord(fileName);
+    fs.mkdirSync(FILE_PATH.componentsPath + '/Business' + `/${fileName}`);
+    copyTemplate(
+        `componentBusiness`,
+        FILE_PATH.componentsPath + '/Business' + `/${fileName}/${fileName}.tsx`,
+        fileName
+    );
 
-  copyTemplate(
-    'componentBusinessLess',
-    FILE_PATH.componentsPath + '/Business' + `/${fileName}/${fileName}.less`,
-    fileName
-  );
+    copyTemplate(
+        'componentBusinessLess',
+        FILE_PATH.componentsPath + '/Business' + `/${fileName}/${fileName}.less`,
+        fileName
+    );
 }
 
 function generate(options, actionName, fileName) {
-  if (!process.cwd().endsWith('/src')) {
-    console.error('请在项目的 src 目录下运行！');
-    return;
-  }
-  if (actionName === ACTION_NAME) {
-    if (fileName.includes('-')) {
-      console.error('pages下文件必须以首字母大写+驼峰命名！');
-      return;
+    console.log("-> process.cwd()", process.cwd());
+    // if (!process.cwd().endsWith('/src')) {
+    //   console.error('请在项目的 src 目录下运行！');
+    //   return;
+    // }
+    // if (actionName === ACTION_NAME) {
+    //   if (fileName.includes('-')) {
+    //     console.error('pages下文件必须以首字母大写+驼峰命名！');
+    //     return;
+    //   }
+    //   //TODO:检查文件名是否重复
+    //   generatePage(fileName);
+    // }
+    if (actionName === 'component' || 'c') {
+        // ag g component basic / business
+        generateComponent(fileName, options);
     }
-    //TODO:检查文件名是否重复
-    generatePage(fileName);
-  }
-  if (actionName === 'component' || 'c') {
-    // ag g component basic / business
-    generateComponent(fileName, options);
-  }
 }
 
 function initCommandGenerate(program) {
-  program
-    .command('generate')
-    .alias('g')
-    .description('generate a page/components/api')
-    .option('-ba, --basic', '创建 basic components')
-    .option('-bu, --business', '创建 business components')
-    .action((options) => {
-      generate(options, ...process.argv.slice(3)); // ag g component
-    });
+    program
+        .command('generate')
+        .alias('g')
+        .description('generate a page/components/api')
+        .option('-ba, --basic', '创建 basic components')
+        .option('-bu, --business', '创建 business components')
+        .action((options) => {
+            generate(options, ...process.argv.slice(3)); // ag g component
+        });
 }
 
 module.exports = initCommandGenerate;
