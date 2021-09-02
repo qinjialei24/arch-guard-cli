b9cf39ab4d96f7f62f97076bd1f62c316bba2810
jialei qin <jialei.qin@thoughtworks.com>
2021-09-02 21:20:23 +0800

    1

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -3,13 +3,6 @@ const path = require('path');
 
 const {toUpperCaseFirstWord, generateFileByTemplate} = require('../utils');
 
-const FILE_PATH = {
-    pagesPath: './pages', //页面存放的路径 TODO: 处理首字母大小写
-    componentsPath: './components', //组件存放的路径，TODO: 处理首字母大小写
-};
-
-const ACTION_NAME = 'page';
-
 function copyTemplate(from, to, fileName, componentName) {
     from = path.join(__dirname, '../templates', from);
     const rawContent = fs.readFileSync(from, 'utf-8');
@@ -17,45 +10,13 @@ function copyTemplate(from, to, fileName, componentName) {
         fileName,
         componentName
     });
-    write(to, finalContent);
-}
-
-function write(path, str, mode) {
-    fs.writeFileSync(path, str);
-}
-
-function generatePage(fileName) {
-    fileName = toUpperCaseFirstWord(fileName);
-    fs.mkdirSync(FILE_PATH.pagesPath + `/${fileName}`);
-    copyTemplate(
-        ACTION_NAME,
-        FILE_PATH.pagesPath + `/${fileName}/${fileName}.tsx`,
-        fileName
-    );
-    copyTemplate(
-        ACTION_NAME + 'Less',
-        FILE_PATH.pagesPath + `/${fileName}/${fileName}.less`,
-        fileName
-    );
-    fs.mkdirSync(FILE_PATH.pagesPath + `/${fileName}` + '/components');
-}
-
-function generateComponent(fileName, componentOptions) {
-    console.log("-> fileName", fileName);
-    const {basic, business} = componentOptions;
-    console.log("-> basic", basic);
-    generateComponentBasic(fileName)
-
-
+    fs.writeFileSync(to, finalContent);
 }
 
 function generateComponentBasic(fileName) {
     const componentName = toUpperCaseFirstWord(fileName)
-
     fs.mkdirSync(fileName)
     fs.mkdirSync(`${fileName}/components`)
-
-
     copyTemplate(
         `componentBasic`,
         `${fileName}/${componentName}.tsx`,
@@ -64,14 +25,14 @@ function generateComponentBasic(fileName) {
     );
 
     copyTemplate(
-      'componentBasicStyle',
-       `${fileName}/${componentName}.style.ts`,
-      fileName
+        'componentBasicStyle',
+        `${fileName}/${componentName}.style.ts`,
+        fileName
     );
 
     copyTemplate(
-      'componentSpec',
-       `${fileName}/${componentName}.spec.ts`,
+        'componentSpec',
+        `${fileName}/${componentName}.spec.ts`,
         '',
         componentName
     );
@@ -94,11 +55,7 @@ function generateComponentBusiness(fileName) {
 }
 
 function generate(options, actionName, fileName) {
-    console.log("-> fileName", fileName);
-    console.log("-> actionName", actionName);
-    console.log("-> process.cwd()", process.cwd());
-    generateComponent(fileName, options);
-
+    generateComponentBasic(fileName)
 }
 
 function initCommandGenerate(program) {
@@ -106,7 +63,7 @@ function initCommandGenerate(program) {
         .command('generate')
         .alias('g')
         .action((options) => {
-            generate(options, ...process.argv.slice(2)); // ag g component
+            generate(options, ...process.argv.slice(2));
         });
 }
 
